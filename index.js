cb9cf8c208762bdc3f257cd446bdbcb5817de53f
Yeison Amado <Yeisonamado811@gmail.com>
2022-01-23 05:40:04 -0500

    ultimo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const { listen } = require('express/lib/application');
 const {dbConnection} = require('./database/config');
-const  {path} = require('path');
+const  path = require('path');
 
 //Configurar CORS
 const cors = require('cors')
@@ -25,7 +25,7 @@ app.use('/api/upload', require('./routes/upload'));
 
 app.get('*',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'public/index.html'));
-})
+});
 app.listen(process.env.PORT,()=>{
     console.log("Servidor corriendo en Servidor "+process.env.PORT);
 })
